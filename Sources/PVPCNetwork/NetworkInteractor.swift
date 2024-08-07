69de9739f4a209fd305527e90a65ea9645decdc3
Bito <tyrion72@gmail.com>
2024-08-07 10:29:48 +0200

    Added docC documentation

--- Sources/PVPCNetwork/NetworkInteractor.swift
+++ Sources/PVPCNetwork/NetworkInteractor.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Alberto Alegre Bravo on 7/8/24.
 //
@@ -14,6 +14,18 @@ public protocol NetworkInteractorProtocol {
 public extension NetworkInteractorProtocol {
     var session: URLSession { .shared }
     
+    
+    /// Obtain a generic type of data that conforms with the Codable protocol. For example, a JSON.
+    ///
+    /// - Parameters:
+    ///   - request: The URLRequest to perform the async call.
+    ///   - type: The type of the data expected to be decoded from the JSON response.
+    ///   - decoder: The JSONDecoder used to decode the JSON data. Default is `JSONDecoder()`.
+    ///
+    ///>Important: You can change the date decode strategy setting on decoder.
+    ///
+    ///
+    /// - Returns: A decoded instance of the specified type.
     func getJSON<JSON>(request: URLRequest, type: JSON.Type, decoder: JSONDecoder = JSONDecoder()) async throws -> JSON where JSON: Codable {
         let (data, response) = try await session.getRequestData(for: request)
         
--- Sources/PVPCNetwork/URLSession.swift
+++ Sources/PVPCNetwork/URLSession.swift
@@ -4,10 +4,16 @@
 import Foundation
 
 public extension URLSession {
+    /// Fetches the data and HTTP response from the specified URL.
+        ///
+        /// - Parameters:
+        ///   - url: The URL from which to fetch data.
+        /// - Returns: A tuple containing the data and the HTTPURLResponse.
     func getData(from url: URL) async throws -> (data: Data, response: HTTPURLResponse) {
         try await getRequestData(for: URLRequest(url: url))
     }
     
+    /// Fetches the data and HTTP response for the specified URL request.
     func getRequestData(for request: URLRequest) async throws -> (data: Data, response: HTTPURLResponse) {
         do {
             let (data, response) = try await data(for: request)
